330370fda06ea01367e19b8805e9f65c811c0b66
Hani Siddiqui <hani.siddiqui@gmail.com>
2022-11-08 15:49:06 -0700

    Added CREATE transition

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -10,12 +10,22 @@ import "./styles.scss";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment (props) {
+  const initialMode = props.interview ? SHOW : EMPTY;
+  const { transition, mode, back } = useVisualMode(initialMode);
+
   return (
     <article className="appointment">
-      <Header time={props.time}/>
-      {props.interview ? <Show student={props.student} interviewer={props.interviewer}/> : <Empty/>}
+      <Header time={props.time}/> 
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
     </article>
   );
 }
